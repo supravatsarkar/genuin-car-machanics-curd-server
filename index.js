6ddc6e649725a18b7ef729fa2aa1a711930bcda2
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-10-26 18:41:13 +0530

    post api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -15,24 +19,18 @@ async function run() {
     try {
         await client.connect();
         console.log('database connected..');
+        const database = client.db('GeniusCarMechanic');
+        const serviceCollection = database.collection('services');
 
         app.post('/services/addServices', async (req, res) => {
-            const database = client.db('GeniusCarMechanic');
-            const serviceCollection = database.collection('services');
-            const services = {
-                name: "Tire Repare",
-                price: 200,
-                time: "30mint",
-                img: "https://i.ibb.co/mFj7kQ5/benjamin-brunner-K3cj-UOMm-Mhc-unsplash.png",
-                description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt ratione in obcaecati nesciunt dolor commodi libero, a, quasi veniam iure consequuntur! At error in facere. Unde voluptate officia a inventore."
-            }
-
-            const result = await serviceCollection.insertOne(services);
-            console.log(result);
+            const service = req.body;
+            console.log(service);
+            const result = await serviceCollection.insertOne(service);
+            res.json(result);
         })
 
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
